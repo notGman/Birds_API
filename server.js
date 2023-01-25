488ced026e25e1934537242a0d959b9d11a2f83c
Gowthaman <gowthaman2185@gmail.com>
2023-01-25 22:00:40 +0530

    error handling fixed

--- server.js
+++ server.js
@@ -10,12 +10,25 @@ fs.readFile("./birds.js", "utf8", function (err, data) {
 app.get('/getbirdname',(req,res)=>{
   res.status(400).send("Search for a bird name!")
 })
+
+app.get('/getbirdname/getallbirds',(req,res)=>{
+  res.status(200).json({
+    data:obj
+  })
+})
+
 app.get("/getbirdname/:data", async (req, res) => {
   const data =  req.params.data;
-  const birdDetail =  obj.filter((bird) => bird.scientific_name == data);
-  res.status(200).json({
-    data: `${birdDetail[0].English_name}`,
-  });
+  try {
+    const birdDetail =  obj.filter((bird) => bird.scientific_name == data);
+    res.status(200).json({
+      data: `${birdDetail[0].English_name}`,
+    });
+  } catch (error) {
+    res.status(400).json({
+      data:[]
+    })
+  }
 });
 
 app.listen(3000, () => console.log("Listening to 3000"));
